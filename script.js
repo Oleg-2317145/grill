03f805eeeb72e234129f104aa2f5a62342ed48d7
Oleg-2317145 <159936871+Oleg-2317145@users.noreply.github.com>
2024-06-12 12:18:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeHeader()
+{
+    let header = document.getElementById("webName");
+    header.innerHTML="Restaurant ";
+    header.style.color="white";
+    header.style.fontSize="50px";
+
+}
+
+function changeHeader1()
+{
+    let header = document.getElementById("webName");
+    header.innerHTML="Grill House ";
+    header.style.fontSize="3em";
+    header.style.color="black";
+    header.style.fontWeight="bold";
+
+}
